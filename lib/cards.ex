d8b4a3450b087036940a5d4bc74b647d483dd858
jason <jason@jasons-thinkpad.attlocal.net>
2020-10-27 21:13:04 -0500

    Added create hand method

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,9 +1,40 @@
 defmodule Cards do
   def create_deck do
-    ["Ace","Two","Three","Four"]
+    values = ["Ace","Two","Three","Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, file_name) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(file_name, binary)
+  end
+
+  def load(file_name) do
+    case File.read(file_name) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "Error loading the file..."
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
   end
 end
